2b5a0c7325dc0a9bb0b95b017c242bd7ec135da3
Макс Клос <klosmax62@gmail.com>
2021-05-17 18:57:24 +0300

    Connecting UserLibrary assembly to ConsoleApp1

---
+++ Lecture1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using UserLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("UserLibrary.dll");
+            Type type = assembly.GetType("UserLibrary.User", true, true);
+            object obj = Activator.CreateInstance(type, new object[] { "Max",18});
+            Console.WriteLine("By ToString override: {0}",obj);
+
+            MethodInfo method = type.GetMethod("DisplayInfo");
+            object result = method.Invoke(obj,new object[]{});
+            Console.WriteLine("By method \"DisplayInfo\": {0}",result);
+                
+            Console.WriteLine("\n-------------------------------------------------");
+            var members = type.GetMembers();
+            foreach (var member in members)
+            {
+                Console.WriteLine(member);
+            }
+
+
+        }
+    }
+
+}
---
+++ Lecture1/UserLibrary/UserLibrary/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UserLibrary
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string DisplayInfo() => $"Name: {Name}, age: {Age}";
+
+        public override string ToString()=>$"Name: {Name}, age: {Age}";
+
+    }
+}
