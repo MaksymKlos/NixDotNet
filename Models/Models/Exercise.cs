10415e5066f7e279db823c23c2e16b5b0332bbbb
Макс Клос <klosmax62@gmail.com>
2021-05-19 01:36:57 +0300

    Added models (User, TrainingProgram,NutritionProgram,Food,Exercise) and links between them

---
+++ Models/Models/Exercise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MuscleGroup { get; set; }
+        public string Description { get; set; }
+
+        public Exercise(string name, string muscleGroup, string description)
+        {
+            Name = name;
+            MuscleGroup = muscleGroup;
+            Description = description;
+        }
+    }
+}
---
+++ Models/Models/NutritionProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class NutritionProgram
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Food> Foods { get; set; }
+
+        public NutritionProgram(string name, string description, List<Food> foods)
+        {
+            Name = name;
+            Description = description;
+            Foods = foods;
+        }
+    }
+}
